c62ff1ea210c6fab20f8351f089c2d6beb5ec82d
Michael <m.anestopoulos24@gmail.com>
2023-02-24 17:28:07 -0500

    adding everything

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+let repeatedString = "";
+const repeatString = function(string, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    for(i = 0; i < num; i++){
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
