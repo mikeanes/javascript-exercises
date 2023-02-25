ec7423d29d07f2c5ab0ca40226ad2c65341e8d6e
Michael <m.anestopoulos24@gmail.com>
2023-02-25 14:34:19 -0500

    finished assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let sum = 0;
+    if(n1 < 0 || n2 < 0){
+        return "ERROR";
+    }
+    else if(typeof n1 != "number" || typeof n2 != "number"){
+        return "ERROR";
+    }else{
+        if(n2 > n1){
+        for(let i = n1; i <= n2; i++){
+            sum += i;
+        }
+            return sum;
+        }else{
+            for(let i = n2; i <= n1; i++){
+            sum += i;
+            }
+            return sum;
+        }
+    }
 };
 
 // Do not edit below this line
